70617e8454749c2b3bbea30fbfa05ffd42a1b36c
harry-pm <harrypenderedmazer@gmail.com>
2019-12-09 10:42:46 +0000

    AddTodo containter created
    the add todo container is created but there is no functionalitly
    
    default react app jsx and css deleted

---
+++ todo-app/src/containers/addTodo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class AddTodo extends React.Component{
+    render() {
+        return (
+            <div className="addTodo">
+                <form /*onSubmit="#"*/ >
+                    <input type="text" /*onChange="#"*/ placeholder="enter todo"></input>
+                    <button>Add</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddTodo
